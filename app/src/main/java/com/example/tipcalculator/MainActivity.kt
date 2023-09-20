57a367862ec797d3909721d18e76e77b184f3412
Justin Cho Chun Wah <chowah67@gmail.com>
2023-09-21 02:10:00 +0800

    Updated Main Activity

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -4,24 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,6 +61,7 @@ fun TipCalculatorApp() {
     var billAmount by remember { mutableStateOf("") }
     var tipPercentage by remember { mutableStateOf(15) }
     var showTipAmount by remember { mutableStateOf(false) }
+    var billAmountError by remember { mutableStateOf(false) }
 
     val keyboardController = LocalSoftwareKeyboardController.current
 
@@ -56,8 +77,11 @@ fun TipCalculatorApp() {
     ) {
         Text (
             text = "Tip Calculator",
-            color = Color.White,
-            fontSize = 32.sp,
+            style = TextStyle (
+                color = Color.White,
+                fontSize = 32.sp,
+                fontFamily = FontFamily(Font(R.font.poppins_semibold))
+            ),
         )
 
         Spacer(modifier = Modifier.height(32.dp))
@@ -65,11 +89,21 @@ fun TipCalculatorApp() {
         OutlinedTextField(
             value = billAmount,
             onValueChange = {
-                billAmount = it
+                if(it.matches(Regex("""^\d*\.?\d*$"""))) {
+                    billAmount = it
+                    billAmountError = false
+                }
+                else {
+                    billAmountError = true
+                }
             },
             label = { Text(
                 text = "Bill Amount",
-                color = Color.White
+                style = TextStyle (
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_semibold))
+                ),
             ) },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
@@ -86,12 +120,25 @@ fun TipCalculatorApp() {
                 unfocusedBorderColor = Color(0xFFb300b3),
                 focusedBorderColor = Color.White
             ),
+            isError = billAmountError,
             shape = RoundedCornerShape(8.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
-
         )
+
+        if(billAmountError) {
+            Text(
+                text = "Invalid input. Please Enter A Valid Number.",
+                style = TextStyle(
+                    color = Color.Red,
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_semibold))
+                ),
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -111,8 +158,11 @@ fun TipCalculatorApp() {
         )
 
         Text(text = "Tip Percentage: $tipPercentage%",
-            fontSize = 16.sp,
-            color = Color.White,
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color.White,
+                fontFamily = FontFamily(Font(R.font.poppins_semibold))
+            ),
             modifier = Modifier
                 .padding(8.dp))
 
@@ -121,10 +171,13 @@ fun TipCalculatorApp() {
         if (showTipAmount) {
             Text(
                 text = "Tip Amount: $${"%.2f".format(tipAmount)}",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(8.dp),
-                color = Color.White
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    fontFamily = FontFamily(Font(R.font.poppins_semibold))
+                ),
             )
 
             Button(
@@ -133,18 +186,41 @@ fun TipCalculatorApp() {
                     billAmount = ""
                     tipPercentage = 15
                 },
-                modifier = Modifier.padding(8.dp)
+                shape = RoundedCornerShape(4.dp),
+                modifier = Modifier
+                    .padding(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White,
+                    containerColor = Color(0xFFb300b3)
+                )
             ) {
-                Text("Reset")
+                Text(
+                    text = "Reset",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontFamily = FontFamily(Font(R.font.poppins_semibold))
+                    ),
+                )
             }
         } else {
             Button(
                 onClick = {
                     showTipAmount = true
                 },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White,
+                    containerColor = Color(0xFFb300b3)
+                ),
+                shape = RoundedCornerShape(4.dp),
                 modifier = Modifier.padding(8.dp)
             ) {
-                Text("Calculate Tip")
+                Text(
+                    text = "Calculate Tip",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontFamily = FontFamily(Font(R.font.poppins_semibold))
+                    ),
+                )
             }
         }
     }
